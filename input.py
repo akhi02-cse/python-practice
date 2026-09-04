acec1d621a655753416a7448b032810a1731d3fb
Akhila <akhi93220@gmail.com>
2026-09-04 19:10:30 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+#input statment:
+#example 1
+name = input("Enter your name: ")
+print("Hello " ,name , sep="!",end="!\n")
+#example 2
+num=int(input("Enter a number: "))
+print("you entered:",num,sep="")
+#example 3
+num1=float(input("Enter a number: "))
+print("value of pi:",num1,sep="")
+
